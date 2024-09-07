1e2a758ab171e73e03d55f1604f85e3661597403
h45h9r11n <nhhanh.2610@gmail.com>
2024-09-07 09:19:20 -0400

    add runtime security

---
+++ falco/falco-install.sh
@@ -0,0 +1,7 @@
+curl -fsSL https://falco.org/repo/falcosecurity-packages.asc | \
+  sudo gpg --dearmor -o /usr/share/keyrings/falco-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/falco-archive-keyring.gpg] https://download.falco.org/packages/deb stable main" | \
+sudo tee -a /etc/apt/sources.list.d/falcosecurity.list
+sudo apt-get update -y
+sudo apt install -y dkms make linux-headers-$(uname -r)
+sudo apt-get install -y falco
